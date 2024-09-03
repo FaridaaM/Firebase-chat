e9e2e648886f3acd28edd23cbd7216c54474562b
FaridaaM <farida.mohamed13@msa.edu.eg>
2024-09-03 23:41:26 +0300

    Initial Commit

---
+++ Firebase chat/Firebase_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_chatApp.swift
+//  Firebase chat
+//
+//  Created by Farida Mohamed on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
